c3957521b3b124973aeb77f8eb448e864df87ddd
Le Tran Thang <81207982+Thang662@users.noreply.github.com>
2026-07-04 18:16:47 +1000

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = float('inf')
+        # self solution, not optimized
+        while l <= r:
+            mid = l + (r - l) // 2
+            time = sum([(pile - 1) // mid + 1 for pile in piles])
+            if time > h:
+                l = mid + 1
+            else:
+                r = mid - 1
+                res = min(res, mid)
+        return res            
