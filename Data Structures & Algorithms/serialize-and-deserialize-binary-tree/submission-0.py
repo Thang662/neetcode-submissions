8055b18824f3abd8db12061a9aa1a67393e3d010
Le Tran Thang <81207982+Thang662@users.noreply.github.com>
2026-07-18 15:57:59 +1000

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,54 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        def dfs(root: Optional[TreeNode]):
+            nonlocal res
+            if not root:
+                res.append('#')
+                return
+            
+            res.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return ','.join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = data.split(',')
+        idx = 0
+        def dfs():
+            nonlocal idx
+            if data[idx] == '#': return
+            
+            root = TreeNode(int(data[idx]))
+            
+            idx += 1
+            root.left = dfs()
+            idx += 1
+            root.right = dfs()
+            return root
+        return dfs()
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
