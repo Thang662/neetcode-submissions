2b61fc609854044fed159bdbac35374bb1b7cd95
Le Tran Thang <81207982+Thang662@users.noreply.github.com>
2026-07-27 08:02:15 +1000

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.'] * n for i in range(n)]
+        res = []
+
+        def dfs(r: int) -> None:
+            if r == n:
+                res.append([''.join(row) for row in board])
+                return
+
+            for i in range(n):
+                if self.check(r, i, board):
+                    board[r][i] = 'Q'
+                    dfs(r+1)
+                    board[r][i] = '.'
+        dfs(0)
+        return res
+        
+    def check(self, r: int, c: int, board: List[List[str]]) -> bool:
+        row = r - 1
+        while row >= 0:
+            if board[row][c] == 'Q':
+                return False
+            row -= 1
+        
+        row, col = r - 1, c - 1
+        while row >= 0 and col >= 0:
+            if board[row][col] == 'Q':
+                return False
+            row, col = row - 1, col - 1
+
+        row, col = r - 1, c + 1
+        while row >= 0 and len(board) > col:
+            if board[row][col] == 'Q':
+                return False
+            row, col = row - 1, col + 1
+        return True
