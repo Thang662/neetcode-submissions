719c1d574f0aa2f18084664e735a9e9f82da535d
Le Tran Thang <81207982+Thang662@users.noreply.github.com>
2026-08-05 16:52:26 +1000

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,23 @@
+from collections import deque, defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjacent = defaultdict(list)
+        indegree = defaultdict(int)
+        queue = deque()
+        finish = 0
+        for a, b in prerequisites:
+            adjacent[a].append(b)
+            indegree[b] += 1
+
+        for i in range(numCourses):
+            if not indegree[i]:
+                queue.append(i)
+
+        while queue:
+            node = queue.popleft()
+            finish += 1
+            for i in adjacent[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    queue.append(i)
+        return finish == numCourses
